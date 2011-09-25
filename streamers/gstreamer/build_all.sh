4269b4ed48d24865e72868c4ed25f4f2855af0b1
offlinehacker <jakahudoklin@gmail.com>
2011-09-25 15:59:39 +0200

    Gstreamer's build_all.sh script now really works.

--- streamers/gstreamer/build_all.sh
+++ streamers/gstreamer/build_all.sh
@@ -2,7 +2,7 @@
 echo "Build start."
 for i in gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-python gst-ffmpeg
 do
-    ./gst-head echo "Compiling $i"
+    source gst-head echo "Compiling $i"
     cd $i
     ./autogen.sh
     make
