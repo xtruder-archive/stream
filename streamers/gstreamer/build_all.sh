57ca8cfb65c4cba85974254a98c3a218db9dd6df
offlinehacker <jakahudoklin@gmail.com>
2011-09-25 15:27:30 +0200

    Changed gstreamer's gst-head and build_all script to work

--- streamers/gstreamer/build_all.sh
+++ streamers/gstreamer/build_all.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+echo "Build start."
 for i in gstreamer gst-plugins-base gst-plugins-good gst-plugins-bad gst-plugins-ugly gst-python gst-ffmpeg
 do
-	cd $i
-	./autogen.sh
-	make
-	cd ..
+    ./gst-head echo "Compiling $i"
+    cd $i
+    ./autogen.sh
+    make
+    cd ..
 done
